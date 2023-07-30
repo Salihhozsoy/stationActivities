002fe613afb1ead6c4745248ece517f2e3e8cf28
salih-ozsoy <salihhozsoy@gmail.com>
2023-07-30 16:04:10 +0300

    last changes

--- app/src/main/java/com/example/stationdesignproject/CalculateActivity.kt
+++ app/src/main/java/com/example/stationdesignproject/CalculateActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Spinner
 
@@ -24,7 +25,7 @@ class CalculateActivity : AppCompatActivity() {
     lateinit var rgLiterOrMoney: RadioGroup
     lateinit var etLiterOrMoney: EditText
 
-    //   var selectedRadioButton: RadioButton? = null
+    var selectedRadioButton: RadioButton? = null
     var calculatedPrice: Double? = null
     var calculatedLiter: Double? = null
 
@@ -39,15 +40,14 @@ class CalculateActivity : AppCompatActivity() {
         spPumps = findViewById(R.id.spPump)
 
 
-        /*        rgLiterOrMoney.setOnCheckedChangeListener { group, checkedId ->                                   //basılan radiobuttonu tutuyor. Full butona basılmadıysa enable
-                    selectedRadioButton = findViewById(checkedId)
-                    selectedRadioButton?.let {
-                        etLiterOrMoney.isEnabled = it.id != R.id.rdbtnFull
-                    } ?: kotlin.run {
-                        showAlert("Warning","Please Choose a Option")
-                    }
-                }
-        */
+        rgLiterOrMoney.setOnCheckedChangeListener { group, checkedId ->                                   //basılan radiobuttonu tutuyor. Full butona basılmadıysa enable
+            selectedRadioButton = findViewById(checkedId)
+            selectedRadioButton?.let {
+                etLiterOrMoney.isEnabled = it.id != R.id.rdbtnFull
+            } ?: kotlin.run {
+                showAlert("Warning", "Please Choose a Option")
+            }
+        }
 
         val carNames = Database.cars.map { it.getListName() }                                             //cars listesini spinner'a basma
         spCars.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, carNames)
@@ -57,12 +57,16 @@ class CalculateActivity : AppCompatActivity() {
 
 
         btnBuyFuel.setOnClickListener {
-            if (!etLiterOrMoney.text.isNullOrEmpty()) {
 
-                val enterAmount = etLiterOrMoney.text.toString().toDouble()
+            if (!etLiterOrMoney.text.isNullOrEmpty() || rgLiterOrMoney.checkedRadioButtonId == R.id.rdbtnFull) {
+                var isError: Boolean = false
 
                 val selectedCarSpinner = Database.cars[spCars.selectedItemPosition]
                 val selectedPumpSpinner = Database.pumps[spPumps.selectedItemPosition]
+                val enterAmountTxt = etLiterOrMoney.text.toString()
+                var enterAmount: Double = 0.0
+
+                if (enterAmountTxt.isNullOrEmpty()) enterAmount = selectedCarSpinner.getEmptyFuel() else enterAmount = etLiterOrMoney.text.toString().toDouble()
 
 
                 if (rgLiterOrMoney.checkedRadioButtonId == R.id.rdbtnLtr) {
@@ -70,36 +74,35 @@ class CalculateActivity : AppCompatActivity() {
                         calculatedLiter = enterAmount
                         calculatedPrice = (enterAmount * selectedCarSpinner.fuelType.price)
                     } else {
+                        isError = true
                         showAlert("Fazla Yakıt", "Depo kapasitesi aşıldı.")
                     }
 
+
                 } else if (rgLiterOrMoney.checkedRadioButtonId == R.id.rdbtnMoney) {
-                    if (enterAmount / selectedCarSpinner.fuelType.price < selectedCarSpinner.capacity - selectedCarSpinner.currentFuelAmount) {
+                    if (enterAmount / selectedCarSpinner.fuelType.price < (selectedCarSpinner.capacity - selectedCarSpinner.currentFuelAmount)) {
                         calculatedLiter = enterAmount / selectedCarSpinner.fuelType.price
                         calculatedPrice = enterAmount
                     } else {
+                        isError = true
                         showAlert("Fazla Para", "Deponuz o kadar almaz")
                     }
+
+
                 } else if (rgLiterOrMoney.checkedRadioButtonId == R.id.rdbtnFull) {
-                    val liter = (selectedCarSpinner.capacity - selectedCarSpinner.currentFuelAmount)
-                    calculatedPrice = (liter * selectedCarSpinner.fuelType.price)
-                    calculatedLiter = liter
-                } else {
-                    showAlert("Uyarı", "Lütfen Bir Seçenek Belirleyin")
+                    calculatedPrice= selectedCarSpinner.getEmptyFuel()*selectedCarSpinner.fuelType.price
+                    calculatedLiter = selectedCarSpinner.getEmptyFuel()
                 }
 
-                val intent = Intent(this, PrintActivity::class.java)
-
-                intent.putExtra(INVOICE, Invoice(2, calculatedPrice!!, calculatedLiter!!, selectedCarSpinner, Database.employee1))
-                intent.putExtra(PUMP, selectedPumpSpinner)
-                startActivity(intent)
-
-            } else {
-                showAlert("Warnings", "Please Enter a Amount")
+                if (!isError) {
+                    val intent = Intent(this, PrintActivity::class.java)
+                    intent.putExtra(INVOICE, Invoice(2, calculatedPrice!!, calculatedLiter!!, selectedCarSpinner, Database.employee1))
+                    intent.putExtra(PUMP, selectedPumpSpinner)
+                    startActivity(intent)
+                }
             }
         }
     }
-
     private fun showAlert(title: String, message: String) {
         AlertDialog.Builder(this)
             .setTitle(title)
--- app/src/main/java/com/example/stationdesignproject/Database.kt
+++ app/src/main/java/com/example/stationdesignproject/Database.kt
@@ -17,12 +17,12 @@ object Database {
     val employee4= Employee(1,"salih")
 
     val pumps = mutableListOf(
-        Pump(1, 1, listOf(FuelType.Diesel,FuelType.Gasoline), employee1),
-        Pump(2, 2, listOf(FuelType.Diesel,FuelType.Electric), employee2),
-        Pump(3, 3, listOf(FuelType.Diesel,FuelType.Gas), employee2),
-        Pump(4, 4, listOf(FuelType.Diesel), employee3),
-        Pump(5, 5, listOf(FuelType.Gas), employee4),
-        Pump(6, 6, listOf(FuelType.Electric), employee4),
+        Pump(1, 1, employee1),
+        Pump(2, 2, employee2),
+        Pump(3, 3, employee2),
+        Pump(4, 4, employee3),
+        Pump(5, 5, employee4),
+        Pump(6, 6, employee4),
     )
 
 }
@@ -30,12 +30,13 @@ object Database {
 data class Car(val id: Int, val name: String, val plateNo: String,val fuelType: FuelType , var currentFuelAmount:Double,val capacity:Double) :
     Parcelable {
     fun getListName() = "MARKA: $name PLAKA: $plateNo"
+    fun getEmptyFuel():Double = capacity-currentFuelAmount
 }
 @Parcelize
 data class Employee(val id:Int, val name: String) : Parcelable
 
 @Parcelize
-data class Pump(val id: Int, val no:Int,val fuelTypes:List<FuelType>,val employee: Employee) :
+data class Pump(val id: Int, val no:Int,val employee: Employee) :
     Parcelable
 @Parcelize
 data class Invoice(val id: Int,val price:Double,val liter:Double, val car: Car, val employee: Employee) :
